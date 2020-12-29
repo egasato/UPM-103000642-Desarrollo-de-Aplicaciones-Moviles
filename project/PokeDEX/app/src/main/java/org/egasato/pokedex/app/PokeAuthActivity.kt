1249b29eadfc28e81355a8e3f538e5131fe48de8
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2020-12-29 18:58:39 +0100

    Add authentication activity

---
+++ project/PokeDEX/app/src/main/java/org/egasato/pokedex/app/PokeAuthActivity.kt
@@ -0,0 +1,12 @@
+package org.egasato.pokedex.app
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * The authentication [activity][AppCompatActivity].
+ *
+ * It allows a user to authenticate and register.
+ *
+ * @author Esaú García Sánchez-Torija
+ */
+class PokeAuthActivity : AppCompatActivity()
