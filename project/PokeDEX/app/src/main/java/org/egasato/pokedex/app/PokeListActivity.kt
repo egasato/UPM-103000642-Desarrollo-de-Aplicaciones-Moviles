26657443bb6ff33fc18e5f277bb5f19ed8b1ec1f
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2020-12-29 18:59:29 +0100

    Add list activity

---
+++ project/PokeDEX/app/src/main/java/org/egasato/pokedex/app/PokeListActivity.kt
@@ -0,0 +1,12 @@
+package org.egasato.pokedex.app
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * The list [activity][AppCompatActivity].
+ *
+ * Shows the list with all the Pokémon.
+ *
+ * @author Esaú García Sánchez-Torija
+ */
+class PokeListActivity : AppCompatActivity()
