cbb0009e935a341933f7b290acf6cee8e00f7f96
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2020-12-29 19:00:30 +0100

    Add details activity

---
+++ project/PokeDEX/app/src/main/java/org/egasato/pokedex/app/PokeDetailsActivity.kt
@@ -0,0 +1,12 @@
+package org.egasato.pokedex.app
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * The details [activity][AppCompatActivity].
+ *
+ * Shows the details of a specific Pokémon.
+ *
+ * @author Esaú García Sánchez-Torija
+ */
+class PokeDetailsActivity : AppCompatActivity()
