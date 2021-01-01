e1f2aa0e14b6173490b3c1e595bc9819b941929a
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2021-01-01 15:50:15 +0100

    Make application keystore-aware

--- project/PokeDEX/app/src/main/java/org/egasato/pokedex/app/PokeApplication.kt
+++ project/PokeDEX/app/src/main/java/org/egasato/pokedex/app/PokeApplication.kt
@@ -1,19 +1,52 @@
 package org.egasato.pokedex.app
 
+import android.app.Activity
 import android.app.Application
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import org.egasato.pokedex.content.PokePreferences
 import org.egasato.pokedex.lib.crypto.PRNG
+import org.egasato.pokedex.lib.security.keystore.Keystore
 
 /**
- * A basic application implementation that performs early fixes and caches objects.
+ * A basic application implementation that caches objects.
  * 
  * @author Esaú García Sánchez-Torija
  */
 class PokeApplication : Application() {
 
+	/** An Android-compatible secure keystore. */
+	val keystore = Keystore(this)
+
+	/** A secure-if-possible shared preferences instance. */
+	lateinit var preferences: PokePreferences
+
 	/** Fixes the pseudo-random number generator on Android 4.3 and below (API <= 18). */
 	override fun onCreate() {
 		super.onCreate()
 		PRNG.fix()
+		preferences = PokePreferences(this)
 	}
-	
+
+	/**
+	 * A component that is aware of the application.
+	 *
+	 * @author Esaú García Sánchez-Torija
+	 */
+	interface Aware {
+
+		/** The context. */
+		val ctx: Context
+			get() = when (this) {
+				is Activity -> application
+				is AppCompatActivity -> application
+				else -> throw RuntimeException("Context-incompatible class")
+			}
+
+		/** The application instance used in this project. */
+		val app: PokeApplication
+			get() = ctx as PokeApplication
+
+	}
+
 }
