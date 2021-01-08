4c0a85b8d5ad30455ec4394c4de73341f6e0675f
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2021-01-08 16:55:56 +0100

    Add token check

--- project/PokeDEX/app/src/main/java/org/egasato/pokedex/app/PokeMainActivity.kt
+++ project/PokeDEX/app/src/main/java/org/egasato/pokedex/app/PokeMainActivity.kt
@@ -2,8 +2,13 @@ package org.egasato.pokedex.app
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import org.egasato.pokedex.R
+import org.egasato.pokedex.lib.security.CryptoException
+import java.io.IOError
+import java.io.IOException
+import javax.crypto.IllegalBlockSizeException
 
 /**
  * The [activity][AppCompatActivity] started by the application launcher.
@@ -23,8 +28,17 @@ class PokeMainActivity : AppCompatActivity(), PokeApplication.Aware {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.splash_background)
-		if (app.keystore.isSupported && !app.keystore.keyPairExists()) app.keystore.generateKeyPair()
-		val intent = if (app.preferences.token.isBlank()) {
+		if (app.keystore.isSupported && !app.keystore.keyPairExists()) {
+			app.keystore.generateKeyPair()
+		}
+		val invalid = try {
+			app.preferences.token.isBlank()
+		} catch (ex: CryptoException) {
+			if (ex.cause !is IOException) throw ex
+			if ((ex.cause as IOException).cause !is IllegalBlockSizeException) throw ex
+			true
+		}
+		val intent = if (invalid) {
 			Intent(this, PokeAuthActivity::class.java)
 		} else {
 			Intent(this, PokeListActivity::class.java)
