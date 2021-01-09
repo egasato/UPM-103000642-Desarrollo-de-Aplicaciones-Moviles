3605bbfcde1a8e7b7835a9fb680e15645f866c79
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2021-01-09 20:50:49 +0100

    Use the list adapter

--- project/PokeDEX/app/src/main/java/org/egasato/pokedex/app/PokeListActivity.kt
+++ project/PokeDEX/app/src/main/java/org/egasato/pokedex/app/PokeListActivity.kt
@@ -1,6 +1,11 @@
 package org.egasato.pokedex.app
 
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import org.egasato.pokedex.R
+import org.egasato.pokedex.model.dm.Pokemon
 
 /**
  * The list [activity][AppCompatActivity].
@@ -9,4 +14,24 @@ import androidx.appcompat.app.AppCompatActivity
  *
  * @author Esaú García Sánchez-Torija
  */
-class PokeListActivity : AppCompatActivity()
+class PokeListActivity : AppCompatActivity() {
+
+	/** The list of Pokémon. */
+	private val pokemons = arrayOfNulls<Pokemon>(1118).toMutableList()
+
+	/** Creates the recycler view adapter. */
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.list_activity)
+		supportActionBar?.apply {
+			setCustomView(R.layout.toolbar)
+			setDisplayShowTitleEnabled(false)
+			setDisplayHomeAsUpEnabled(false)
+			displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+		}
+		findViewById<RecyclerView>(R.id.list).also {
+			it.adapter = PokeListAdapter(this, pokemons)
+		}
+	}
+
+}
