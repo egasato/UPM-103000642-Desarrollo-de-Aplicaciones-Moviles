1b4ca6cc17c310cff3bcec2b6179347b28169e4a
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2021-01-09 20:30:16 +0100

    Add loading view holder

---
+++ project/PokeDEX/app/src/main/java/org/egasato/pokedex/view/animation/PokeLoadingViewHolder.kt
@@ -0,0 +1,12 @@
+package org.egasato.pokedex.view.animation
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * The view holder of the Pokémon loading layout.
+ *
+ * @param view The view.
+ * @author Esaú García Sánchez-Torija
+ */
+class PokeLoadingViewHolder(view: View) : RecyclerView.ViewHolder(view)
