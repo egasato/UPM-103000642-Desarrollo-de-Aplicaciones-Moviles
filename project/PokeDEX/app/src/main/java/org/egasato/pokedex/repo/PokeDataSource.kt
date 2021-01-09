1f009a0cb22ac178a7646e09367de52dd86090fc
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2021-01-09 01:55:27 +0100

    Add Pokémon data source

---
+++ project/PokeDEX/app/src/main/java/org/egasato/pokedex/repo/PokeDataSource.kt
@@ -0,0 +1,49 @@
+package org.egasato.pokedex.repo
+
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.google.gson.Gson
+import org.egasato.pokedex.log.PokeLogger
+import org.egasato.pokedex.model.dto.NetworkPokeListRequest
+import org.egasato.pokedex.model.dto.NetworkPokeListResponse
+
+/** The Kotlin logger object. */
+private val logger = PokeLogger.logger {}
+
+/**
+ * The data source used for the Pokédex.
+ *
+ * @author Esaú García Sánchez-Torija
+ */
+object PokeDataSource {
+
+	/** The base URL of the Pokédex API. */
+	private const val base = "https://pokeapi.co/api/v2"
+
+	/** The [GSON][Gson] object. */
+	private val gson = Gson()
+
+	/**
+	 * Performs a synchronous Pokémon list request.
+	 *
+	 * @param request The request object.
+	 * @return The response.
+	 */
+	fun pokemon(request: NetworkPokeListRequest): NetworkPokeListResponse? {
+		val req = AndroidNetworking.get("$base/pokemon")
+			.addQueryParameter("limit", request.limit.toString())
+			.addQueryParameter("offset", request.offset.toString())
+			.setPriority(Priority.LOW)
+			.build()
+		logger.event { "Requesting: ${req.url}" }
+		val res = req.executeForString()
+		return if (res.isSuccess) {
+			logger.event("Parsing response as NetworkPokeListResponse")
+			gson.fromJson(res.result as String, NetworkPokeListResponse::class.java)
+		} else {
+			logger.event("Parsing response failed")
+			null
+		}
+	}
+
+}
