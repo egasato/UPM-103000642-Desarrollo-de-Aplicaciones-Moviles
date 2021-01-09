20212f105f4a92b723b932d4cc80b34e4c4bdc32
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2021-01-09 20:47:02 +0100

    Fix API-returned names

--- project/PokeDEX/app/src/main/java/org/egasato/pokedex/model/map/PokeListResponseMapper.kt
+++ project/PokeDEX/app/src/main/java/org/egasato/pokedex/model/map/PokeListResponseMapper.kt
@@ -4,6 +4,10 @@ import org.egasato.pokedex.model.dm.PokeListResponse
 import org.egasato.pokedex.model.dm.Pokemon
 import org.egasato.pokedex.model.dto.NetworkPokeListResponse
 import org.egasato.pokedex.model.dto.NetworkPokeResource
+import java.util.Locale
+
+/** The regular expression used with the dashed names. */
+private val DASHES = Regex("-([^-]+)")
 
 /**
  * Maps a login response as a DTO network model to a domain model.
@@ -24,7 +28,12 @@ object PokeListResponseMapper : Mapper<NetworkPokeListResponse, PokeListResponse
 			list = input.results.mapIndexed { i: Int, v: NetworkPokeResource ->
 				Pokemon.builder().apply {
 					id = offset + i + 1
-					name = v.name
+					name = v.name.capitalize(Locale.ROOT)
+						.replace("Mr-", "Mr. ")
+						.replace(DASHES) { " (${it.groupValues[1]})" }
+						.replace(" (oh)", "-oh")
+						.replace("(m)", " (male)")
+						.replace("(f)", " (female)")
 					image = null
 					stats = null
 				}.build()
