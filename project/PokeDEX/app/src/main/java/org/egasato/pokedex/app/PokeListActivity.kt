4c125014edd44e1f91d33acf1ff311a06cba8d21
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2021-01-10 17:52:37 +0100

    Add animators and use view model

--- project/PokeDEX/app/src/main/java/org/egasato/pokedex/app/PokeListActivity.kt
+++ project/PokeDEX/app/src/main/java/org/egasato/pokedex/app/PokeListActivity.kt
@@ -1,19 +1,46 @@
 package org.egasato.pokedex.app
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import org.egasato.pokedex.R
+import org.egasato.pokedex.lib.util.Pair
+import org.egasato.pokedex.lib.util.Single
+import org.egasato.pokedex.lib.view.animation.CompatValueAnimator
+import org.egasato.pokedex.lib.widget.TileImageView
+import org.egasato.pokedex.lifecycle.PokeViewModel
 import org.egasato.pokedex.log.PokeLogger
-import org.egasato.pokedex.model.dm.Pokemon
 import org.egasato.pokedex.recyclerview.PokeListAdapter
+import org.egasato.pokedex.view.animation.MovementAnimator
+import org.egasato.pokedex.view.animation.OffsetAnimator
+import org.egasato.pokedex.view.animation.ScaleAnimator
+import java.util.concurrent.TimeUnit
+import kotlin.math.cos
+import kotlin.math.sin
 
 /** The Kotlin logger object. */
 private val logger = PokeLogger.logger {}
 
 /** The complete name of the class. */
 private val CLASS = PokeListActivity::class.java.canonicalName
+
+/** The minimum scale. */
+private const val MIN_SCALE = 2f
+
+/** The maximum scale. */
+private const val MAX_SCALE = 4f
+
+/** The scale delays. */
+private val DELAY_SCALE = arrayOf(40L, 50L).map {
+	TimeUnit.MILLISECONDS.convert(it, TimeUnit.SECONDS)
+}.map(Single.Companion::create)
+
+/** The movement delays. */
+private val DELAY_MOVEMENT = arrayOf(15L, 30L).map {
+	TimeUnit.MILLISECONDS.convert(it, TimeUnit.SECONDS)
+}.map(Single.Companion::create)
 
 /**
  * The list [activity][AppCompatActivity].
@@ -24,8 +51,35 @@ private val CLASS = PokeListActivity::class.java.canonicalName
  */
 class PokeListActivity : AppCompatActivity() {
 
-	/** The list of Pokémon. */
-	private val pokemons = arrayOfNulls<Pokemon>(1118).toMutableList()
+	/** The view model of the authentication activity. */
+	private val model by viewModels<PokeViewModel>()
+
+	/** The bitmap scale value. */
+	private lateinit var scaleBmp: Single<Float>
+
+	/** The drawing scale value. */
+	private lateinit var scaleDraw: Single<Float>
+
+	/** The maximum scale value. */
+	private lateinit var scaleMax: Single<Float>
+
+	/** The minimum scale value. */
+	private lateinit var scaleMin: Single<Float>
+
+	/** The movement values. */
+	private lateinit var movement: Pair<Float, Float>
+
+	/** The offset values. */
+	private lateinit var offset: Pair<Float, Float>
+
+	/** The scale animator. */
+	private lateinit var scaleAnimator: CompatValueAnimator
+
+	/** The movement animator. */
+	private lateinit var movementAnimator: CompatValueAnimator
+
+	/** The scale animator. */
+	private lateinit var offsetAnimator: CompatValueAnimator
 
 	// Logs the object creation
 	init {
@@ -48,12 +102,108 @@ class PokeListActivity : AppCompatActivity() {
 			displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
 		}
 
+		// Initialize the view model or update the activity if already initialized (orientation change)
+		initViewModelOrUpdateActivity()
+
 		logger.android("Configuring the adapter")
 		findViewById<RecyclerView>(R.id.list).also {
 			it.setHasFixedSize(true)
 			it.itemAnimator?.changeDuration = 0
-			it.adapter = PokeListAdapter(this, pokemons)
+			it.adapter = PokeListAdapter(this, model.pokemons)
 		}
+	}
+
+	/** Initializes the view model or updates the activity state with it.  */
+	private fun initViewModelOrUpdateActivity() {
+		logger.android("Initializing view model or updating the activity state")
+		logger.android("Synchronizing the background image state")
+		findViewById<TileImageView>(R.id.background).also {
+			if (!model.first) {
+				logger.android("Initializing the view model (scale properties)")
+				model.scaleBmp = Single.create(it.scaleBitmap.coerceAtLeast(MAX_SCALE))
+				model.scaleDraw = Single.create(it.scaleDraw.coerceAtMost(1f))
+				model.scaleMin = Single.create(MIN_SCALE)
+				model.scaleMax = Single.create(MAX_SCALE)
+			}
+			scaleBmp = model.scaleBmp
+			scaleDraw = model.scaleDraw
+			scaleMax = model.scaleMax
+			scaleMin = model.scaleMin
+		}.also {
+			if (!model.first) {
+				logger.android("Initializing the view model (movement properties)")
+				model.movement = Pair.create(0f, 0f).apply {
+					val direction = Math.random() * Math.toRadians(360.0)
+					first = cos(direction).toFloat()
+					second = sin(direction).toFloat()
+				}
+			}
+			movement = model.movement
+		}.also {
+			if (!model.first) {
+				logger.android("Initializing the view model (offset properties)")
+				model.offset = Pair.create(0f, 0f).apply {
+					first = it.offsetX
+					second = it.offsetY
+				}
+			}
+			offset = model.offset
+		}.also(::initAnimators)
+		model.first = true
+	}
+
+	/**
+	 * Initializes the animators.
+	 *
+	 * @param background The background image view.
+	 */
+	private fun initAnimators(background: TileImageView) {
+		logger.android("Creating the animators")
+		scaleAnimator = ScaleAnimator(
+			scaleBmp = scaleBmp, scaleDraw = scaleDraw,
+			maxScale = scaleMax, minScale = scaleMin,
+			minDelay = DELAY_SCALE[0], maxDelay = DELAY_SCALE[1]
+		)
+		movementAnimator = MovementAnimator(
+			movement = movement,
+			minDelay = DELAY_MOVEMENT[0], maxDelay = DELAY_MOVEMENT[1]
+		)
+		offsetAnimator = OffsetAnimator(
+			offset = offset,
+			movement = movement,
+			scale = object : Single<Float> {
+				override var first: Float
+					get() = scaleDraw.first * scaleBmp.first
+					set(_) {}
+			}
+		).apply {
+			observable.subscribe {
+				background.scaleDraw = scaleDraw.first
+				background.offsetX += offset.first
+				background.offsetY += offset.second
+				background.invalidate()
+			}
+		}
+	}
+
+	/** Starts the animators. */
+	override fun onResume() {
+		logger.android("Resuming the activity")
+		super.onResume()
+		logger.android("Resuming the animators")
+		scaleAnimator.play()
+		movementAnimator.play()
+		offsetAnimator.play()
+	}
+
+	/** Stops the animators. */
+	override fun onDestroy() {
+		logger.android("Stopping the animators")
+		scaleAnimator.stop()
+		movementAnimator.stop()
+		offsetAnimator.stop()
+		logger.android("Destroying the activity")
+		super.onDestroy()
 	}
 
 }
